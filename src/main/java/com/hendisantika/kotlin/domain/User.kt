479698fb522a69e16ab2a0c0ba86df0144e52822
Hendi Santika <hendisantika@gmail.com>
2018-09-22 07:00:58 +0700

    #10 - Add User Domain

--- src/main/java/com/hendisantika/kotlin/domain/User.kt
+++ src/main/java/com/hendisantika/kotlin/domain/User.kt
@@ -1,14 +1,8 @@
 package com.hendisantika.kotlin.domain
 
 import java.io.Serializable
-import java.util.HashSet
-import javax.persistence.CascadeType
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.FetchType
-import javax.persistence.Id
-import javax.persistence.OneToMany
-import javax.persistence.Table
+import java.util.*
+import javax.persistence.*
 
 /*
 
