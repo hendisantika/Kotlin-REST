edb86c2e8458d261e54799ea69238ce502451581
Hendi Santika <hendisantika@gmail.com>
2018-09-22 07:02:24 +0700

    #14 - Add User Service

--- src/main/java/com/hendisantika/kotlin/service/UserService.kt
+++ src/main/java/com/hendisantika/kotlin/service/UserService.kt
@@ -10,8 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
-import java.util.ArrayList
-import java.util.HashSet
+import java.util.*
 
 /*
 
